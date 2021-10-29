88c6f17e4db49fff6ff0d5e3a27481638888a5b6
zhangao <zhangao@yuanfudao.com>
2021-10-29 14:47:38 +0800

    feat(rust): solved 66

---
+++ problems/rust-solutions/src/66.加一.rs
@@ -0,0 +1,23 @@
+/**
+ * 66.加一 [Easy]
+ * Time Complexity: O(n^2), Space Complexity: O(1)
+ * https://leetcode-cn.com/problems/plus-one/
+ */
+
+impl Solution {
+  pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+    let mut digits = digits;
+    for i in (0..digits.len()).rev() {
+      if digits[i] != 9 {
+        digits[i] += 1;
+        return digits;
+      }
+      digits[i] = 0;
+      if i == 0 {
+        digits.insert(0, 1);
+        return digits;
+      }
+    }
+    return digits;
+  }
+}
